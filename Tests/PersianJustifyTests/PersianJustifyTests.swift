78d70d00b76a3e24031aa9cdf4d617b2a8b7a2f6
Seyed Mojtaba Hosseini Zeidabadi <Chenzook+GitHub@gmail.com>
2024-03-14 21:51:41 +0330

    test: add a simple test for `UITextView.getFont`

--- Tests/PersianJustifyTests/PersianJustifyTests.swift
+++ Tests/PersianJustifyTests/PersianJustifyTests.swift
@@ -22,4 +22,14 @@ final class PersianJustifyTests: XCTestCase {
         XCTAssertEqual(sut.getFont(), expFont)
         XCTAssertEqual(sut.getFont(), sut.font)
     }
+
+    func testUITextViewGetFont() {
+        let sut = UITextView()
+        let expFont = UIFont.systemFont(ofSize: 100)
+        sut.font = expFont
+
+        XCTAssertEqual(sut.font, expFont)
+        XCTAssertEqual(sut.getFont(), expFont)
+        XCTAssertEqual(sut.getFont(), sut.font)
+    }
 }
