f8f10814b90b14a6d3b1e3b0f0e2e05c0f687d06
Seyed Mojtaba Hosseini Zeidabadi <Chenzook+GitHub@gmail.com>
2024-03-14 22:13:59 +0330

    feat: add the `AppKit` version of the `getFont`

--- Sources/Extensions/View.getFont.swift
+++ Sources/Extensions/View.getFont.swift
@@ -6,3 +6,12 @@ internal extension UIView {
     func getFont() -> UIFont? { value(forKey: "font") as? UIFont }
 }
 #endif
+
+#if canImport(AppKit)
+import AppKit
+
+internal extension NSView {
+
+    func getFont() -> NSFont? { value(forKey: "font") as? NSFont }
+}
+#endif
