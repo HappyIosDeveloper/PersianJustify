2eff7e1c9cd99e6a8b78eb8d68aebb7520b38482
Seyed Mojtaba Hosseini Zeidabadi <Chenzook+GitHub@gmail.com>
2024-03-15 00:03:53 +0330

    feat: use the typealias to make it ready for the `AppKit`

--- Sources/PersianJustify.swift
+++ Sources/PersianJustify.swift
@@ -20,10 +20,10 @@ fileprivate let forbiddenExtendableCharacters = ["ا", "د", "ذ", "ر", "ز", "
 // MARK: - Usage using toPJString function
 extension String {
     
-    public func toPJString(in view: UIView) -> NSAttributedString {
+    public func toPJString(in view: View) -> NSAttributedString {
         //        return self // MARK: Uncomment to see the unjustified text
         if isEmpty { return NSAttributedString(string: self) }
-        let defaultFont = UIFont()
+        let defaultFont = Font()
         let font = view.getFont() ?? defaultFont
         let final = NSMutableAttributedString(string: "")
         let allLines = replacingOccurrences(of: "\n\n", with: nextLineCharacter.description).getWords(separator: nextLineCharacter)
@@ -53,7 +53,7 @@ extension String {
 // MARK: - Private Functions
 private extension String {
     
-    func getJustifiedLine(in parentWidth: CGFloat, isLastLineInParagraph: Bool, font: UIFont) -> NSMutableAttributedString {
+    func getJustifiedLine(in parentWidth: CGFloat, isLastLineInParagraph: Bool, font: Font) -> NSMutableAttributedString {
         let words = getWords(separator: spaceCharacter)
         let totalWordsWidth = words.compactMap({$0.getWordWidth(font: font)}).reduce(0, +)
         let emptySpace = parentWidth - totalWordsWidth
@@ -82,7 +82,7 @@ private extension String {
         }
     }
     
-    func getExtendedWords(words: [String], requiredExtend: CGFloat, font: UIFont) -> NSMutableAttributedString {
+    func getExtendedWords(words: [String], requiredExtend: CGFloat, font: Font) -> NSMutableAttributedString {
         print("------------------------------------------")
         let style = NSMutableParagraphStyle()
         style.alignment = NSTextAlignment.justified
@@ -109,11 +109,11 @@ private extension String {
         return split(separator: separator).compactMap({$0.description})
     }
 
-    func getTotalWidth(in view: UIView) -> CGFloat {
+    func getTotalWidth(in view: View) -> CGFloat {
         return view.frame.width
     }
     
-    func getWordWidth(font: UIFont, isRequiredSpace: Bool = true) -> CGFloat {
+    func getWordWidth(font: Font, isRequiredSpace: Bool = true) -> CGFloat {
         let text = isRequiredSpace ? (self + spaceCharacter.description) : self
         let attributedString = NSAttributedString(string: text, attributes: [.font: font])
         let line = CTLineCreateWithAttributedString(attributedString)
@@ -144,7 +144,7 @@ private extension String {
 
 private extension [String] {
     
-    func hasRoomForNextWord(nextWord: String, parentWidth: CGFloat, font: UIFont) -> Bool {
+    func hasRoomForNextWord(nextWord: String, parentWidth: CGFloat, font: Font) -> Bool {
         let requiredWidth = nextWord.getWordWidth(font: font)
         let currentWidth = compactMap({$0.getWordWidth(font: font)}).reduce(0, +)
         return (currentWidth + requiredWidth) <= parentWidth
