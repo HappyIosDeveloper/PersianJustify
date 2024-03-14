7a83c42d6ea6ee5023bfaab4187869330acb2045
Seyed Mojtaba Hosseini Zeidabadi <Chenzook+GitHub@gmail.com>
2024-03-14 22:18:50 +0330

    feat: define helper typealias to be more clear about the current context

--- Sources/Extensions/View.getFont.swift
+++ Sources/Extensions/View.getFont.swift
@@ -1,5 +1,17 @@
 #if canImport(UIKit)
 import UIKit
+typealias Font = UIFont
+typealias View = UIView
+#endif
+
+#if canImport(AppKit)
+import AppKit
+typealias Font = NSFont
+typealias View = NSView
+#endif
+
+#if canImport(UIKit)
+import UIKit
 
 internal extension UIView {
 
