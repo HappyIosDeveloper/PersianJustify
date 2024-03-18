081c98b64fc93bf78119d8926f1a8242b79e5e60
Sajad Vishkai <sajaclvishkai@gmail.com>
2024-03-18 10:30:23 +0100

    Remove comments.

--- Package.swift
+++ Package.swift
@@ -6,18 +6,18 @@ import PackageDescription
 let package = Package(
     name: "PersianJustify",
     products: [
-        // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
             name: "PersianJustify",
-            targets: ["PersianJustify"]),
+            targets: ["PersianJustify"]
+        ),
     ],
     targets: [
-        // Targets are the basic building blocks of a package, defining a module or a test suite.
-        // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "PersianJustify"),
+            name: "PersianJustify"
+        ),
         .testTarget(
             name: "PersianJustifyTests",
-            dependencies: ["PersianJustify"]),
+            dependencies: ["PersianJustify"]
+        ),
     ]
 )
