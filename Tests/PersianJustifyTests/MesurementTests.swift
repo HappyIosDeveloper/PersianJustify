cc4c4bd0110b677bcd70631e9b8297466d1aee95
Sajad Vishkai <sajaclvishkai@gmail.com>
2024-03-18 11:01:28 +0100

    Introduce `MeasurementTests ` `AppKit ` test case.

--- Tests/PersianJustifyTests/MesurementTests.swift
+++ Tests/PersianJustifyTests/MesurementTests.swift
@@ -18,3 +18,22 @@ final class MeasurementTests: XCTestCase {
     }
 }
 #endif
+
+#if canImport(AppKit)
+final class MeasurementTests: XCTestCase {
+    func testPerformance() throws {
+        self.measure(metrics: [XCTCPUMetric(), XCTMemoryMetric()]) {
+            let text1 = ""
+            let text2 = "blah blah"
+            let text3 = "السلام اللعیکم\nو رحمت الله و برکاتو"
+
+            let sut = NSTextView()
+            sut.textStorage?.append(text1.toPJString(in: sut))
+
+            sut.textStorage?.append(text2.toPJString(in: sut))
+
+            sut.textStorage?.append(text3.toPJString(in: sut))
+        }
+    }
+}
+#endif
