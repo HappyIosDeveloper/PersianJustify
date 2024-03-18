3b80603655c038c4f90c4b2a581864e35602851c
Sajad Vishkai <sajaclvishkai@gmail.com>
2024-03-18 10:31:19 +0100

    Revisit file in order to improve readability.

--- Sources/Extensions/View.getFont.swift
+++ Sources/Extensions/View.getFont.swift
@@ -1,17 +1,22 @@
 #if canImport(UIKit)
 import UIKit
-public typealias Font = UIFont
-public typealias View = UIView
 #elseif canImport(AppKit)
 import AppKit
-public typealias Font = NSFont
-public typealias View = NSView
 #endif
 
-internal extension View {
-    func getFont() -> Font? { 
-        let key = "font"
-        guard responds(to: Selector(key)) else { return nil }
+private let key = "font"
+
+extension View {
+    func getFont() -> Font? {
+        // Opaque pointer to font in the given view
+        let fontSelector = Selector(key)
+
+        // Arguably this defensive check can be omitted,
+        // Since if a view has a font property, we can fetch font from 
+        guard responds(to: fontSelector) else {
+            return nil
+        }
+
         return value(forKey: key) as? Font
     }
 }
