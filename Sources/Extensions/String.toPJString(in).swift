533996cd65b830a6f7971fe3d2afc86944744f14
Seyed Mojtaba Hosseini Zeidabadi <Chenzook+GitHub@gmail.com>
2024-03-18 17:02:22 +0330

    style: encapsulate deprecated interface and its dependencies into a separate file

---
+++ Sources/Extensions/String.toPJString(in).swift
@@ -0,0 +1,21 @@
+import Foundation
+
+extension String {
+
+    @available(
+        *, deprecated,
+        renamed: "toPJString(fittingWidth:font:)",
+        message: "This method requires too much information and will not be available from v1.0"
+    )
+    public func toPJString(in view: View) -> NSAttributedString {
+        let defaultFont = Font()
+        let font = view.getFont() ?? defaultFont
+        let parentWidth = getTotalWidth(in: view)
+
+        return toPJString(fittingWidth: parentWidth, font: font)
+    }
+
+    func getTotalWidth(in view: View) -> CGFloat {
+        return view.frame.width
+    }
+}
