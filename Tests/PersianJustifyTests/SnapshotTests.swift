db9f184c3917fa27265e3bde544a4d0baa65e35b
Seyed Mojtaba Hosseini Zeidabadi <Chenzook+GitHub@gmail.com>
2024-03-20 01:09:17 +0330

    feat!: mark both simple snapshot tests as `recording` to make the test failing
    There is a bug on the last line that needs to be fixed

--- Tests/PersianJustifyTests/SnapshotTests.swift
+++ Tests/PersianJustifyTests/SnapshotTests.swift
@@ -33,7 +33,7 @@ extension SnapshotTests {
             sut.numberOfLines = 0
             sut.textColor = .black
 
-            assertSnapshot(of: sut, as: .image(size: CGSize(width: width, height: 780)))
+            assertSnapshot(of: sut, as: .image(size: CGSize(width: width, height: 780)), record: true)
         }
     }
 }
@@ -49,7 +49,7 @@ extension SnapshotTests {
             let sut = NSTextField(labelWithAttributedString: justifiedText)
             sut.textColor = .black
 
-            assertSnapshot(of: sut, as: .image(size: CGSize(width: width, height: 780)))
+            assertSnapshot(of: sut, as: .image(size: CGSize(width: width, height: 780)), record: true)
         }
     }
 }
