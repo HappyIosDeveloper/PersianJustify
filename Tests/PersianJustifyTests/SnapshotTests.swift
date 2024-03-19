c3e0de747f6902904fda867bef80154bf01b660c
Seyed Mojtaba Hosseini Zeidabadi <Chenzook+GitHub@gmail.com>
2024-03-20 01:08:15 +0330

    test: add a simple case for the NSTextField with a long justified text

--- Tests/PersianJustifyTests/SnapshotTests.swift
+++ Tests/PersianJustifyTests/SnapshotTests.swift
@@ -21,6 +21,20 @@ extension SnapshotTests {
 
         assertSnapshot(of: sut, as: .image(size: CGSize(width: 360, height: 780)))
     }
+
+    func testLongMultilineTextOnUILabel() throws {
+        let width: CGFloat = 360
+        for fontName in FontBlaster.loadedFonts {
+            let font = try XCTUnwrap(Font(name: fontName, size: 17))
+            let justifiedText = shortDemoText.toPJString(fittingWidth: width, font: font)
+            let sut = UILabel()
+            sut.attributedText = justifiedText
+            sut.numberOfLines = 0
+            sut.textColor = .black
+
+            assertSnapshot(of: sut, as: .image(size: CGSize(width: width, height: 780)))
+        }
+    }
 }
 #endif
 
