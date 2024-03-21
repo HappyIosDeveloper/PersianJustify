26adb6385c1cebe16150205c0d4428774ebe7c36
Sajad Vishkai <sajaclvishkai@gmail.com>
2024-03-21 12:16:44 +0100

    Revert "Introduce new method `linesProcessing`."
    This reverts commit 937b3c51471d7abb969ac682f64b67a3d7772ab8.

--- Sources/PersianJustify.swift
+++ Sources/PersianJustify.swift
@@ -106,18 +106,6 @@ extension String {
         return final
     }
 
-    private func linesProcessing(lines: [Line]) -> (index: Int, line: [Word]) {
-        var line = [Word]()
-        var index = 0
-
-        lines.enumerated().lazy.forEach {
-            line = $1.getWords()
-            index = $0
-        }
-
-        return (index, line)
-    }
-
     private func justifyLine(
         from words: [Word],
         in proposedWidth: CGFloat,
