15a76218723564717851de7800511ccb7ad88b64
Sajad Vishkai <sajaclvishkai@gmail.com>
2024-03-21 12:44:09 +0100

    Specify target path.

--- Package.swift
+++ Package.swift
@@ -17,7 +17,8 @@ let package = Package(
     ],
     targets: [
         .target(
-            name: "PersianJustify"
+            name: "PersianJustify",
+            path: "Sources"
         ),
         .testTarget(
             name: "PersianJustifyTests",
