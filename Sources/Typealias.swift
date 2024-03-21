5358f931cff321e15a1f5769c9c5b47031a899e4
Sajad Vishkai <sajaclvishkai@gmail.com>
2024-03-21 13:33:30 +0100

    Add a new typealias named `Label`.

--- Sources/Typealias.swift
+++ Sources/Typealias.swift
@@ -7,9 +7,11 @@ import UIKit
 
 public typealias Font = UIFont
 public typealias View = UIView
+public typealias Label = UILabel
 #elseif canImport(AppKit)
 import AppKit
 
 public typealias Font = NSFont
 public typealias View = NSView
+public typealias Label = NSTextView
 #endif
